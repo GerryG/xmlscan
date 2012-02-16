a94139971adf14285a5a5339e9abb24f12ee7f9e
Gerry Gleason <gerrg@inbox.com>
2012-02-16 11:40:39 -0600

    Moving more details into options and parameters

--- xmlcard.rb
+++ xmlcard.rb
@@ -15,7 +15,11 @@ module MyVisitor
       class_eval %{def #{i}(d, *a) d&&(@out << d) end}, __FILE__, __LINE__
     end
 
-    def initialize()
+    def initialize(opts={})
+      @element = opts[:element] || raise("need an element")
+      @key = opts[:key] || raise("need a key")
+      @extras = (ex = opts[:extras]) ? ex.map(&:to_sym) : []
+
       @pairs = []    # output [name, content, value] * 1 or more
       @context = ''  # current key(name) of the element (card)
       @stack = []    # stack of containing context cards
@@ -26,28 +30,30 @@ module MyVisitor
 
     def on_chardata(s) @out << s end
     def on_stag_end(name, s, h, *a)
-      if name.to_sym == :card
+      if name.to_sym == @element
         # starting a new context, first output our substitute string
-        key= h&&h['name']||'*no-name*'
+        key= h&&h[@key.to_s]||'*no-name*'
         sub = h['transclude'] || "{{#{key}}}"
         @out << sub
         # then push the current context and initialize this one
-        @stack.push([@context, @out, @type])
-        @context = key; @out = []; @type = h['type']
+        @stack.push([@context, @out, *@ex])
+        @context = key; @out = []
+        @ex = @extras.map {|e| h[e]}
       else @out << s end # pass through tags we aren't processing
     end
 
     def on_etag(name, s=nil)
-      if name.to_sym == :card
+      if name.to_sym == @element
         # output a card (name, content, type)
-        @pairs << [@context, @out, @type, @stack[-1][0]]
+        @pairs << [@context, @out, @stack[-1][0], *@ex]
         # restore previous context from stack
-        @context, @out, @type = (@stack.pop)
+        last = @stack.pop
+        @context, @out, @ex = last.shift, last.shift, *last
       else @out << s end
     end
 
     def on_stag_empty_end(name, s=nil, h={}, *a)
-      if name.to_sym == :card
+      if name.to_sym == @element
         # I don't think we have this case, but it is simple to add later
         STDERR << "empty card ???: #{name}, #{s}, #{h.inspect}\n"
       else @out << s end
@@ -59,10 +65,10 @@ module MyVisitor
   class MyProcessor
     include ElementProcessor
 
-    def self.process(file)
+    def self.process(file, opts={})
       raise "Not readable #{file.inspect}" unless IO===( io =
                                              IO===file ? file : open(file) )
-      visitor = new
+      visitor = new(opts)
       visitor.parser.parse(io)
       visitor.pairs
     end
@@ -71,9 +77,9 @@ module MyVisitor
 end
 
 ARGV.each do |a|
-  pairs = MyVisitor::MyProcessor.process(a)
+  pairs = MyVisitor::MyProcessor.process(a, {:key=>:name, :element=>:card, :extras=>[:type]})
   STDOUT << "Result\n"
-  STDOUT << pairs.map do |p| n,o,t,c = p
+  STDOUT << pairs.map do |p| n,o,c,t = p
       "#{c&&c.size>0&&"#{c}::"||''}#{n}#{t&&"[#{t}]"}=>#{o*''}"
     end * "\n"
   STDOUT << "\nDone\n"

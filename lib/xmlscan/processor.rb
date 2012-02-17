95c630da0996a6046fd2c9c5591b9159904010e3
Gerry Gleason <gerrg@inbox.com>
2012-02-16 18:45:59 -0600

    0.3.0prec, I think this one will be usable for Wagn

--- lib/xmlscan/processor.rb
+++ lib/xmlscan/processor.rb
@@ -1,6 +1,7 @@
 # encoding: UTF-8
 require 'xmlscan/parser'
 require 'xmlscan/visitor'
+require 'stringio'
 
 module XMLScan
   module ElementProcessor
@@ -14,20 +15,20 @@ module XMLScan
 
     def initialize(opts={}, mod=nil)
       raise "No module" unless mod
-      STDERR << "init Element Processer #{mod}\n"
       (MY_METHODS - mod.instance_methods).each do |i|
-        self.class.class_eval %{def #{i}(d, *a) d&&(@out << d) end}, __FILE__, __LINE__
+        self.class.class_eval %{def #{i}(d, *a) d&&(self << d) end}, __FILE__, __LINE__
       end
       self.class.send :include, mod
 
       @element = opts[:element] || raise("need an element")
       @key = opts[:key] || raise("need a key")
       @extras = (ex = opts[:extras]) ? ex.map(&:to_sym) : []
+      @tmpl = opts[:substitute] || "{{:key}}"
 
-      @pairs = []    # output [name, content, value] * 1 or more
-      @context = ''  # current key(name) of the element (card)
-      @stack = []    # stack of containing context cards
-      @out = []      # current output for name(card)
+      @pairs = {}   # output name=> [content, context, extra_values] * 1 or more
+      @context = '' # current key(name) of the element (card)
+      @stack = []   # stack of containing context cards
+      @out = []     # current output for name(card)
       @parser = XMLScan::XMLParser.new(self)
       self
     end
@@ -41,8 +42,8 @@ module XMLScan
       mod ||= ElementProcessing
       STDERR << "process #{io.inspect}, #{opts.inspect}\n"
       io = case io
-          when String; open(io)
           when IO, StringIO; io
+          when String; open(io)
           else raise "bad type file input #{io.inspect}"
         end
 
@@ -54,36 +55,31 @@ module XMLScan
 
 
   module ElementProcessing
-    def on_chardata(s) @out << s end
+    def <<(s) @out << s end
+    def on_chardata(s) self << s end
     def on_stag_end(name, s, h, *a)
       if name.to_sym == @element
         # starting a new context, first output our substitute string
         key= h&&h[@key.to_s]||'*no-name*'
-        @tmpl = ":transclude|{{:name}}" # def: "{{:key}}"
-        STDERR << "templ #{@tmpl.inspect}\n"
-        #STDERR << "x> #{x.inspect}, #{h.inspect}, #{(!(/:\w[\w\d]*/ =~ x)) || h[$&[1..-1].to_s] }\n"
-        sub =
-          @tmpl.split('|').find {|x| !(/:\w[\w\d]*/ =~ x) ||
-          h[$&[1..-1].to_s] }.gsub(/:\w[\w\d]*/) {|m|
-            STDERR << "templ sub match #{m.inspect}, #{h[m[1..-1]]}\n"
-            h[m[1..-1]] }
-        #sub = h['transclude'] || "{{#{key}}}"
-        @out << sub
+        self << @tmpl.split('|').find {
+          |x| !(/:\w[\w\d]*/ =~ x) || h[$&[1..-1].to_s] }.gsub(/:\w[\w\d]*/) {
+            |m| h[m[1..-1]]
+         }
         # then push the current context and initialize this one
         @stack.push([@context, @out, *@ex])
-        @context = key; @out = []
-        @ex = @extras.map {|e| h[e]}
-      else @out << s end # pass through tags we aren't processing
+        @pairs[key] = nil # insert it when first seen
+        @context = key; @out = []; @ex = @extras.map {|e| h[e.to_s]}
+      else self << s end # pass through tags we aren't processing
     end
 
     def on_etag(name, s=nil)
       if name.to_sym == @element
         # output a card (name, content, type)
-        @pairs << [@context, @out, @stack[-1][0], *@ex]
+        @pairs[@context] = [@out, @stack[-1][0], *@ex]
         # restore previous context from stack
         last = @stack.pop
         @context, @out, @ex = last.shift, last.shift, *last
-      else @out << s end
+      else self << s end
     end
 
     def on_stag_empty_end(name, s=nil, h={}, *a)
@@ -91,19 +87,11 @@ module XMLScan
 
         key= h&&h[@key.to_s]||'*no-name*'
         ex = @extras.map {|e| h[e]}
-        @pairs << [key, [], @context, *ex]
-      else @out << s end
+        @pairs[key] = [[], @context, *ex]
+      else self << s end
     end
 
     attr_reader :pairs, :parser
   end
 
-end
-ARGV.each do |a|
-  pairs = XMLScan::XMLProcessor.process(a, {:key=>:name, :element=>:card, :extras=>[:type]}, XMLScan::ElementProcessing)
-  STDOUT << "Result\n"
-  STDOUT << pairs.map do |p| n,o,c,t = p
-      "#{c&&c.size>0&&"#{c}::"||''}#{n}#{t&&"[#{t}]"}=>#{o*''}"
-    end * "\n"
-  STDOUT << "\nDone\n"
 end
--- test/integration/xmlcard.rb
+++ test/integration/xmlcard.rb
@@ -3,9 +3,10 @@
 require 'xmlscan/processor'
 
 ARGV.each do |a|
-  pairs = XMLScan::XMLProcessor.process(a, {:key=>:name, :element=>:card, :extras=>[:type]})
+  pairs = XMLScan::XMLProcessor.process(a, {:key=>:name, :element=>:card,
+            :extras=>[:type], :substitute=>':transclude|{{:name}}'})
   STDOUT << "Result\n"
-  STDOUT << pairs.map do |p| n,o,c,t = p
+  STDOUT << pairs.map do |p| n=p[0]; o,c,t = p[1]
       "#{c&&c.size>0&&"#{c}::"||''}#{n}#{t&&"[#{t}]"}=>#{o*''}"
     end * "\n"
   STDOUT << "\nDone\n"
